83b072f99272367cc43e7dbb2b2f6fc20ef7e683
Danilo Faraum <112991335+DaniloFaraum@users.noreply.github.com>
2023-11-17 08:51:38 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const min = document.getElementById('min')
 const sec = document.getElementById('sec')
 const musica = document.getElementById('musica')
-const foco = document.getElementById('foco')
 const curto = document.getElementById('curto')
 const longo = document.getElementById('longo')
 const comecar = document.getElementById('comecar')
@@ -16,42 +15,28 @@ let isActive = false
 
 const checkTimer = () => {
     if (isActive == true){
-        comecar.textContent = "cancelar"
-        comecar.addEventListener('click', resetTimer)
+        comecar.textContent = "start"
+        isActive = false
+        time = minTimer * 25 /1000
     }
     else{
+        comecar.textContent = "cancelar"
         startTimer()
     }
 }
 
 const startTimer = () => {
-    if (isActive == false) {
-        isActive = true
-        checkTimer()
-
-        const timerInterval = setInterval(() => {
-            let minutes = Math.floor(time / 60)
-            let seconds = time % 60
-            min.textContent = minutes
-            sec.textContent = seconds
-            if (time <= 0 ) {
-                clearInterval(timerInterval)
-            } else {
-                time--
-            }
-        }, segTimer)
-}
-    else{
-             // clearInterval(timerInterval)
-              isActive = false
-              time = minTimer * 25 /1000
-    }
-}
-
-const resetTimer = () =>{
-    if (isActive == true){
-  //  clearInterval(timerInterval)
-    isActive = false
-    time = minTimer * 25 /1000
-    }
+    isActive = true
+    const timerInterval = setInterval(() => {
+        let minutes = Math.floor(time / 60)
+        let seconds = time % 60
+        min.textContent = minutes
+        sec.textContent = seconds
+        if (time <= 0 || isActive == false) {
+            clearInterval(timerInterval)
+        } 
+        else {
+            time--
+        }
+    }, segTimer)
 }
